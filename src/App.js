47123e222191b397fef33f51b193b1e8b0ff314a
Dmytro Fitsych <dimaficych@gmail.com>
2023-07-05 21:52:28 +0200

    Connected API

--- src/App.js
+++ src/App.js
@@ -1,54 +1,20 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import React from "react";
+import { useEffect } from "react";
+
+const APIURL = "http://www.omdbapi.com?apikey=f57c1a45";
+const searchMovies = async (tittle) => {
+  const response = await fetch(`${APIURL}&s=${tittle}`);
+  const data = await response.json();
+
+  console.log(data.Search);
+};
+
+useEffect(() => {
+  searchMovies("Spiderman");
+}, []);
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    setCounter(100);
-  }, []);
-
-  // Props
-  // const Person = (props) => {
-  //   return (
-  //     <>
-  //       <h1>Name: {props.name}</h1>
-  //       <h2>Last name: {props.lastName}</h2>
-  //       <h2>Age: {props.age}</h2>
-  //     </>
-  //   );
-  // };
-
-  // const name = "Dmytro";
-  // const isNameShowing = false;
-
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
-        -
-      </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
-        +
-      </button>
-
-      {/* props */}
-      {/* <Person name={"Dmytro"} lastName={"Fitsych"} age={"26"} />
-      <Person name="Mary" lastName="Fitsych" age="27" />
-      <Person />
-      <Person /> */}
-
-      {/* <h1>Hello, {isNameShowing ? name : "someone"}</h1>
-      {isNameShowing ? (
-        <>{name}</>
-      ) : (
-        <>
-          <h1>test</h1>
-          <h2>There is no name</h2>
-        </>
-      )} */}
-    </div>
-  );
+  return <></>;
 };
 
 export default App;
