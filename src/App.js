a2e69967489221de864c73f2f1864b0362087141
Dmytro Fitsych <dimaficych@gmail.com>
2023-07-05 21:24:22 +0200

    promps, useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(100);
+  }, []);
+
+  // Props
+  // const Person = (props) => {
+  //   return (
+  //     <>
+  //       <h1>Name: {props.name}</h1>
+  //       <h2>Last name: {props.lastName}</h2>
+  //       <h2>Age: {props.age}</h2>
+  //     </>
+  //   );
+  // };
+
+  // const name = "Dmytro";
+  // const isNameShowing = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
+
+      {/* props */}
+      {/* <Person name={"Dmytro"} lastName={"Fitsych"} age={"26"} />
+      <Person name="Mary" lastName="Fitsych" age="27" />
+      <Person />
+      <Person /> */}
+
+      {/* <h1>Hello, {isNameShowing ? name : "someone"}</h1>
+      {isNameShowing ? (
+        <>{name}</>
+      ) : (
+        <>
+          <h1>test</h1>
+          <h2>There is no name</h2>
+        </>
+      )} */}
     </div>
   );
-}
+};
 
 export default App;
