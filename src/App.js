c206e03963d214ff2b9ab2cb944d6fd10b4788c0
Dmytro Fitsych <dimaficych@gmail.com>
2023-07-06 10:01:04 +0200

    Added movieCard

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
-import { useEffect } from "react";
 
-const APIURL = "http://www.omdbapi.com?apikey=f57c1a45";
-const searchMovies = async (tittle) => {
-  const response = await fetch(`${APIURL}&s=${tittle}`);
-  const data = await response.json();
-
-  console.log(data.Search);
+const movie1 = {
+  Title: "Italian Spiderman",
+  Year: "2007",
+  imdbID: "tt2705436",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
 };
 
-useEffect(() => {
-  searchMovies("Spiderman");
-}, []);
+const APIURL = "http://www.omdbapi.com?apikey=f57c1a45";
 
 const App = () => {
-  return <></>;
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (tittle) => {
+    const response = await fetch(`${APIURL}&s=${tittle}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input placeholder="Search for movies" onChange={() => {}} />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
